00d8dc726382f5d1d94f501ac5f457588b6da3e8
Lewis Henson <lewishenson@users.noreply.github.com>
2020-09-25 11:20:37 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TimeZoneDemo
+{
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Time Zone demo");
+
+            var systemTimeZones = TimeZoneInfo.GetSystemTimeZones();
+            Console.WriteLine($"{systemTimeZones.Count} time zones present");
+
+            Console.WriteLine();
+
+            // Valid for Windows
+            GetTimeZone("GMT Standard Time");
+
+            Console.WriteLine();
+
+            // Valid for Linux
+            GetTimeZone("Europe/London");
+        }
+
+        static void GetTimeZone(string timeZoneId)
+        {
+            try
+            {
+                Console.WriteLine($"Getting time zone {timeZoneId}...");
+
+                var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+                Console.WriteLine($"{timeZoneInfo.DisplayName} ({timeZoneInfo.StandardName})");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unable to get time zone {timeZoneId}!");
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
